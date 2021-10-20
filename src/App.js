dcaeea9a028fe2cbe37d0ad2576893add3cb2104
Gustavo Andrade <gustavo_alex3000@hotmail.com>
2021-10-20 15:57:27 -0300

    feat: add file structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className>YOoooooooooo</h1>
+      <h1 className="text-red-500">YOoooooooooo</h1>
     </div>
   );
 }
